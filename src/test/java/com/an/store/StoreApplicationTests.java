68aa2723566d8c678565468546e6067fbf87532d
wangabeng <wangabeng@163.com>
2019-08-06 13:22:17 +0800

    init

---
+++ src/test/java/com/an/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.an.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
